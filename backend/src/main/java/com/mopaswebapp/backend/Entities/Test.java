d49e8d4234ccf279419a4bb305cd893d06c78ffd
akbacosman <akbacosman@gmail.com>
2021-10-05 18:19:06 +0300

    Test class was created on devop

---
+++ backend/src/main/java/com/mopaswebapp/backend/Entities/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public void setTest_name(Integer test_name) {
+        this.test_name = test_name;
+    }
+
+    public Test(Integer test_name) {
+        this.test_name = test_name;
+    }
+
+    public Integer getTest_name() {
+        return test_name;
+    }
+
+    private Integer test_name ;
+}
